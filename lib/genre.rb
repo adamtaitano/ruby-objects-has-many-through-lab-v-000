401f80952fbd5cbcc7648777b24a96f586439a46
Adam Taitano <adam.taitano@gmail.com>
2018-09-14 18:38:16 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,3 +1,20 @@
 class Genre
-
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
+  
 end
