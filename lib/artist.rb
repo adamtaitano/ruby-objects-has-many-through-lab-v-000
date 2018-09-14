55a70e077d8387a88d62c5b2e1e102ce2fb451c6
Adam Taitano <adam.taitano@gmail.com>
2018-09-14 18:31:28 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,32 @@
 class Artist
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @all
+  end
+  
+  def self.new_song(song_name)
+    new_song = Song.new(song_name)
+    new_song.artist = self
+  end
+  
+  def self.songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
+  
+  def self.genres
+    own_songs = self.songs
+    own_songs.select do |song|
+      song.genre
+    end
+  end
 
 end
